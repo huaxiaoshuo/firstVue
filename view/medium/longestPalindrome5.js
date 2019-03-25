27bf5420ee4a1f0c7be9f41535033fcb92a30add
huonaibo <huonaibo@rouchi.com>
2019-03-25 20:14:24 +0800

    第五题

--- view/medium/longestPalindrome5.js
+++ view/medium/longestPalindrome5.js
@@ -1,14 +1,35 @@
 
 // 正在写
-var longestPalindrome = function(s) {
-  let resultArr = []
-  let returnArr = function (index) {
-    if (index > s.length) return
-    for (let a = 0; a < s.length; a++) {
+var longestPalindrome = function (s) {
+  let start = 0, end = 0;
+  if (s == null || s.length < 1) return '';
+  for (let i = 0; i < s.length; i++) {
+      let len1 = expand(s, i, i)
+      let len2 = expand(s, i, i + 1)
+      console.log(len1, len2)
+      let len = Math.max(len1, len2)
+      if (len > end - start + 1) {
+        if (len % 2) {
+          start = i - (len - 1) / 2
+          end = i + (len - 1) / 2
+        } else {
+          start = i - (len - 2) / 2
+          end = i + 1 + (len - 2) / 2
+        }
+        console.log(start, end)
+      }
 
-    }
   }
-  returnArr(0)
-
+  console.log(start, end)
+  return s.substr(start, end + 1)
+  function expand (s, left, right) {
+    let L = left, R = right;
+    while (L >= 0 && R < s.length && s.charAt(L) === s.charAt(R)) {
+      L--;
+      R++;
+    }
+    return R - L - 1
+  }
 };
-longestPalindrome('babad')
+let result = longestPalindrome("cbbd")// babaab
+console.error(result)
